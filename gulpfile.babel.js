2efeb6b6fed34022afaf2bf270fe1eb60962bc91
Lucas Ruy <lukas.r@hotmail.com>
2018-02-25 12:24:26 -0300

    adicionado gulp-svg-sprite criada tarefa para gerar sprite de svg, onde todos arquivos SVG inclusos em "app/static/images/svg" são adicionado no sprite

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import requireDir from 'require-dir';
 export const directories = {
   app: {
     images: './app/static/images/*',
+    svg: './app/static/images/svg/*.svg',
     templates: 'app/static/templates/index.pug',
     stylesheets: 'app/static/stylesheets/application.styl',
     javascripts: 'app/src/app.js',
--- tasks/images.js
+++ tasks/images.js
@@ -1,10 +1,30 @@
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
+import svg from 'gulp-svg-sprite';
 
 import { directories } from '../gulpfile.babel';
 
-gulp.task('images', () => {
+gulp.task('images:files', () => {
   gulp.src(directories.app.images)
     .pipe(imagemin())
     .pipe(gulp.dest(directories.public.images));
 });
+
+gulp.task('images:svg', () => {
+  return gulp.src(directories.app.svg)
+  .pipe(svg({
+    mode: {
+      symbol: {
+        render: {
+          css: false,
+          scss: false
+        },
+        dest: 'svg',
+        sprite: 'icons.svg',
+      }
+    }
+  }))
+  .pipe(gulp.dest(directories.public.images));
+});
+
+gulp.task('images', ['images:files', 'images:svg']);
