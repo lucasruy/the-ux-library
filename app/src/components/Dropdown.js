955fc0c07f6ca06568b32ec228903f60330e3069
Lucas Ruy <lukas.r@hotmail.com>
2018-02-28 00:06:54 -0300

    correções recomendadas do linter adicionada correções baseado no ESLint

--- app/src/components/Dropdown.js
+++ app/src/components/Dropdown.js
@@ -1,13 +1,13 @@
 const Dropdown = () => {
   const list = document.querySelectorAll('.menu-dropdown');
 
-  const enableDropdown = function() {
+  const enableDropdown = function dropdown() {
     this.classList.toggle('is-active');
   };
 
-  list.forEach( (element) => {
+  list.forEach((element) => {
     element.addEventListener('click', enableDropdown, false);
-  } );
+  });
 };
 
 export default Dropdown;
